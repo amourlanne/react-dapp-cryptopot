81a48e943398adad79d5790f2bbbb1e348595ff7
Alexis Mourlanne <alexis.mourlanne@etu.u-bordeaux.fr>
2019-06-28 17:27:18 +0200

    add reducer

--- src/redux/reducers/Moneypot/reducer.js
+++ src/redux/reducers/Moneypot/reducer.js
@@ -2,6 +2,7 @@ import {moneypotConstants} from "../../constants/Moneypot";
 
 const initialState = {
   moneypots: [],
+  current: null
 };
 
 export function moneypotReducer(state = initialState, action) {
@@ -15,7 +16,8 @@ export function moneypotReducer(state = initialState, action) {
   case moneypotConstants.MONEYPOTS_INITIALIZED:
     return {
       ...state,
-      moneypots: payload.moneypots
+      moneypots: payload.moneypots,
+      current: payload.moneypots.shift()
     };
   default:
     return state;
